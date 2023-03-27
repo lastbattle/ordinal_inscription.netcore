4bc097f93ece43a7129d68ce1c46501bd1cc7628
lastbattle <4586194+lastbattle@users.noreply.github.com>
2023-03-27 12:37:48 +0800

    Initial commit
    - supports decoding of ordinal data from witness script.
    - api query to blockchain.info

---
+++ goat.netcore.test/UnitTest1.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Nito.AsyncEx;
+using System.Diagnostics;
+
+namespace goat.netcore.test {
+
+    [TestClass]
+    public class UnitTest1 {
+
+        [TestMethod]
+        public void Test_BitcoinCoreRPC() {
+            //Goat g = new Goat();
+            //g.ProcessRawTx("167b24f615b9c35c39064e314adc4fdb802ed1050ecf649ce887859ee3c5f6db");
+
+
+        }
+
+        [TestMethod]
+        public void Test_BlockchainInfoRPC() {
+            Goat g = new Goat();
+
+            AsyncContext.Run(async () => {
+                OrdinalData ordinal = await g.CreateAPIQuery("167b24f615b9c35c39064e314adc4fdb802ed1050ecf649ce887859ee3c5f6db", BcDataProviderType.BlockchainInfo);
+                if (ordinal != null) {
+                    Debug.WriteLine(ordinal);
+                }
+            });
+        }
+    }
+}
